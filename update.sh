960ec1f35e43ee70df0e1c9839c027d43dec6764
Danny Bendelman <danny@hivecollective.co>
2018-04-29 15:53:01 -0400

    Initial commit

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+current_branch="$(git symbolic-ref --short -q HEAD)"
+
+cp -f Dockerfile.template Dockerfile.generated
+sed -i "s#?ALERTMANAGER_VERSION?#$current_branch#g" Dockerfile.generated
+
+if [ ! -f Dockerfile ] || ! cmp -s Dockerfile.generated Dockerfile; then
+    rm Dockerfile 2>/dev/null || true
+    mv Dockerfile.generated Dockerfile
+    git reset
+    git add Dockerfile
+    git commit -m "Updated version to $current_branch"
+else
+    rm Dockerfile.generated
+fi
